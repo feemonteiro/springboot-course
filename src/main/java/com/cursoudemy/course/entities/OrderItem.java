791d6a9ae7eb759a830e7e00288c5bbf7f8985d6
Fernanda Monteiro de Almeida <feemoonteiro@gmail.com>
2022-07-28 10:31:44 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/cursoudemy/course/entities/OrderItem.java
+++ src/main/java/com/cursoudemy/course/entities/OrderItem.java
@@ -42,7 +42,8 @@ public class OrderItem implements Serializable{
 		id.setOrder(order);
 	}
 	
-	public Product productOrder() {
+	//@JsonIgnore
+	public Product getProduct() {
 		return id.getProduct();
 	}
 	
