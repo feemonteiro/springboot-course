3257b930aaf2d966d2602b5e33f3a4f59051be52
Fernanda Monteiro de Almeida <feemoonteiro@gmail.com>
2022-07-29 14:40:47 -0300

    exception handling - delete

--- src/main/java/com/cursoudemy/course/services/UsersService.java
+++ src/main/java/com/cursoudemy/course/services/UsersService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cursoudemy.course.entities.Users;
 import com.cursoudemy.course.repositories.UsersRepository;
+import com.cursoudemy.course.services.exceptions.DatabaseException;
 import com.cursoudemy.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,16 @@ public class UsersService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public Users update(Long id, Users obj) {
