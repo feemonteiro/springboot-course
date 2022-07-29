47d49c61f42c3d4b46849f8b0e5dd5f2d8e82e12
Fernanda Monteiro de Almeida <feemoonteiro@gmail.com>
2022-07-29 14:47:44 -0300

    Exception hadling - update

--- src/main/java/com/cursoudemy/course/services/UsersService.java
+++ src/main/java/com/cursoudemy/course/services/UsersService.java
@@ -3,6 +3,8 @@ package com.cursoudemy.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -47,9 +49,15 @@ public class UsersService {
 	}
 	
 	public Users update(Long id, Users obj) {
-		Users entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			Users entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+			
 	}
 
 	private void updateData(Users entity, Users obj) {
